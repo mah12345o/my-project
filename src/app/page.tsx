bcfa2e97acc783f1312b3acd31a4104c9e6c851b
mah12345o <maheshsuthar083@gmail.com>
2025-06-04 18:00:26 +0530

    refactor: comment out 'Learn' link for future use

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -38,7 +38,7 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn
+          {/* Learn */}
         </a>
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
